b95444e56be5aac761e5b4007d741c7f223a1b89
Srikar Thottempudi <srikarthottempudi@Srikars-MacBook-Air.local>
2019-04-17 14:47:25 +0530

    storing data in documents

---
+++ Sandbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sandbox
+//
+//  Created by Srikar Thottempudi on 4/16/19.
+//  Copyright © 2019 Srikar Thottempudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
